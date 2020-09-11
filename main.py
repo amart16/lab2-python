571649ba1c8dd0d26afbaf06f2afa4ce276c0cd1
amart16 <arm6554@psu.edu>
2020-09-11 03:56:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Author: Alex Martin arm6554psu.edu
+# Collaborator: jjb6692@psu.edu
+# Collaborator:
+# Collaborator:
+# Section: 
+# Breakout: 
+
+def getLetterGrade(grade):
+  if grade >= 93.0:
+    return "A"
+  elif grade >= 90.0:
+    return "A-"
+  elif grade >= 87.0:
+    return "B+"
+  elif grade >= 83.0:
+    return "B"
+  elif grade >= 80.0:
+    return "B-"
+  elif grade >= 77.0:
+    return "C+"
+  elif grade >= 70.0:
+    return "C"
+  elif grade >= 60.0:
+    return "D"
+  elif grade < 60:
+    return "F"
+def run():
+  grade = float(input("CMPSCI 131 grade: "))
+  print(f"Your letter grade is {getLetterGrade(grade)}.")
+
+if __name__ == "__main__":
+  run()
